aee6a4cfea6afc982afb0b30359869fe79d8bf98
alexrossi73 <alessio_rossi@hotmail.com>
2018-08-23 18:19:44 +0200

    Create it.js

---
+++ src/locale/it.js
@@ -0,0 +1,16 @@
+export default {
+	effects: {
+		captions: {
+			blur: 'Blur',
+			crop: 'Taglia',
+			enhance: 'Migliora',
+			flip: 'Rifletti',
+			grayscale: 'Scala di grigio',
+			invert: 'Inverti',
+			mirror: 'Specchio',
+			rotate: 'Ruota',
+			sharp: 'Nitidezza',
+		},
+		apply: 'Applica',
+	},
+}
